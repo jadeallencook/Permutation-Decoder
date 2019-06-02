b2307af1ff52590ac1c812405f140325f4709d49
Jade Allen Cook <jadeallencook@gmail.com>
2019-06-02 11:47:20 -0400

    🔧 Refactored decoder

--- decoder.js
+++ decoder.js
@@ -1,22 +1,15 @@
 const decoder = string => {
-
-    const sequence = string.split('').map(num => parseInt(num));
-    const length = sequence.length;
-    let result = (length) ? [1, 1, ...Array(length - 1).fill(0)] : null;
-
-    if (!length || length === 1) return 1;
-    else if (sequence[0] === 0) return 0;
-    
-    for (let index = 2; index <= length; index++) {
-        const num = parseInt(`${sequence[index - 2]}${sequence[index - 1]}`);
-        result[index] += (
-            num >= 10 && 
-            num <= 26 && 
-            num % 10 !== 0
-        ) ? result[index - 2] + result[index - 1] : result[index - 1];
-    }
-
-    return result[length];
+    let previous = result = 1;
+    if (string.startsWith('0')) return 0;
+    string.split('').map((value, index) => {
+        if (index) {
+            const num = parseInt(`${string[index - 1]}${value}`);
+            const canPermutate = (num >= 10 && num <= 26 && num % 10 !== 0);
+            result = (canPermutate) ? result + previous : result;
+            previous = (canPermutate) ? result - previous : result;
+        }
+    });
+    return result;
 }
 
 module.exports = decoder;
