aa65fd5de26a329befe1d7110c5ea78f08eef981
Jade Allen Cook <jadeallencook@gmail.com>
2019-06-02 21:21:17 -0400

    🔧 Decoder refactoring and more tests added

--- decoder.js
+++ decoder.js
@@ -1,14 +1,15 @@
 const decoder = string => {
-    let previous = result = 1;
-    if (string.startsWith('0')) return 0;
-    string.split('').map((value, index) => {
+    let previous = (string.endsWith('0')) ? 0 : 1, result = 1;
+    if (string.startsWith('0') || string.indexOf('00') !== -1) return 0;
+    for (const [index, value] of string.split('').entries()) {
         if (index) {
             const num = parseInt(`${string[index - 1]}${value}`);
             const canPermutate = (num >= 10 && num <= 26 && num % 10 !== 0);
+            if (num % 10 === 0 && num > 26) return 0;
             result = (canPermutate) ? result + previous : result;
             previous = (canPermutate) ? result - previous : result;
         }
-    });
+    }
     return result;
 }
 
--- decoder.test.js
+++ decoder.test.js
@@ -23,3 +23,19 @@ test('"2012" returns 2', () => {
 test('"1212121212" returns 89', () => {
     expect(decoder('1212121212')).toBe(89);
 });
+
+test('"100" returns 0', () => {
+    expect(decoder('100')).toBe(0);
+});
+
+test('"110" returns 1', () => {
+    expect(decoder('110')).toBe(1);
+});
+
+test('"230" returns 0', () => {
+    expect(decoder('230')).toBe(0);
+});
+
+test('"301" returns 0', () => {
+    expect(decoder('301')).toBe(0);
+});
